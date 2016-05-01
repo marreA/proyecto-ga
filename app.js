b8dd1bfc1a11f42c62f6f055c57d21d0d46452f1
marrero- <alexmd54@gmail.com>
2016-05-01 17:18:58 +0100

    Añadidas algunas plantillas para comenzar el proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+let port = process.env.PORT || 8080;
+let ip = process.env.IP || '127.0.0.1';
+let addr =  `${ip}:${port}`;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+  response.render('index', {title : 'Proyecto Procesadores de Lenguajes'});
+});
+
+app.listen(port,ip, () =>{
+   console.log(`App listening at ${addr}`);
