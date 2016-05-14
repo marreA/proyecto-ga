8e2a2bcb472c9970f8ba0542e1df37caee93c9ab
Alejandro Marrero <alexmd54@gmail.com>
2016-05-14 15:43:33 +0000

    Cambio en block

--- public/js/block.js
+++ public/js/block.js
@@ -7,4 +7,8 @@
       this.variables = variables;
       this.functions = functions;
     };
+    Block.prototype = Object.create(AbstactType.prototype);
+    Block.prototype.constructor = Block;
+    
+    module.exports = Block;
 })();
