bf2a2e83f508a01843b77e304132ecb3c0583734
Alejandro Marrero <alexmd54@gmail.com>
2016-05-17 09:16:16 +0000

    Se ha cambiado las definición let por const

--- models/semantic.js
+++ models/semantic.js
@@ -2,7 +2,7 @@
 (() => {
     "use strict";
     //   La fase de análisis semántico recibe el árbol AST de la fase sintáctica
-    let semantic = (tree) => {
+    const semantic = (tree) => {
         let emptyTable = {}; //  Creamos una tabla de símbolos vacía
         //  Hacemos un recorrido en preorden para construir la tabla
         eachBlockPre(tree, buildTable, emptyTable);
@@ -12,14 +12,14 @@
     //  Recibe un nodo del árbol
     //  La acción a realizar
     //  El padre del nodo actual
-    let eachBlockPre = (tree, action, father) => {
+    const eachBlockPre = (tree, action, father) => {
         action(tree,father);    //  Ejecutamos la acción sobre el nodo
         //  El árbol es un objeto que tiene un atributo funciones y para cada una de ellas construimos su tabla de símbolos
         tree.functions.forEach((func) => eachBlockPre(func, action, tree.symbolTable));
     };
     
     //  Función para construir la tabla de símbolos
-    let buildTable = (block, f) => {
+    const buildTable = (block, f) => {
         //  Definimos cual es el padre de cada uno de los bloques dentro de su tabla de símbolos
         block.symbolTable = {
             father: f
@@ -37,7 +37,7 @@
     
     //  Creamos el método que nos permite insertar un símbolo dentro de la tabla de símbolos
     //  Recibe el símbolo en el argumento value y la tabla de símbolos en el segundo argumento
-    let insertSymbol = (value, symbolTable) => {
+    const insertSymbol = (value, symbolTable) => {
         if (value instanceof Array) {
             if (symbolTable[value[0]])
                 console.error("Error: " + value[0] + " declared yet.");
